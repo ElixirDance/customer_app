440885d8f973ebc2bf915e0ecf1fdfcb302b7d11
xiongyechang <a226791767@gmail.com>
2025-06-13 16:47:33 +0800

    feat: 修改名称

--- src/pages/demo/index.vue
+++ src/pages/demo/index.vue
@@ -1,6 +1,6 @@
 <template>
     <up-cell-group>
-        <up-cell title="儿童专属档案" isLink url="/pages/childArchive/index?id=1"></up-cell>
+        <up-cell title="儿童专属档案" isLink url="/pages/childManage/index?id=1"></up-cell>
         <up-cell title="站内搜索" isLink url="/pages/siteSearch/index"></up-cell>
         <up-cell title="家庭管理" isLink url="/pages/familyManage/index"></up-cell>
         <up-cell title="分析" isLink url="/pages/analytics/index"></up-cell>
